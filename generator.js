ad6dcd53b600a6af363a5f0083f39c5958b9857d
sicheo <66950550+sicheo@users.noreply.github.com>
2026-04-24 15:50:56 +0200

    added host and hostname to generator

--- generator.js
+++ generator.js
@@ -88,12 +88,13 @@ const baseConfig = {
   "param_strain_CTE":25, // coeff. dilatazione termica sensore
   "param_strain_Lfal":0.1385, // lunghezza di montaggio sensore in m
   "development" : false, // flag per sviluppo o esercizio
-  "ota_proxy_host": "10.147.131.28", // Hostname o IP del proxy per OTA update
+  "ota_proxy_host": "10.147.131.28",
+  "ota_proxy_hostname": "onesait-device-proxy-col.coll.opencs.servizi.prv",
   "ota_proxy_port": 443, // Porta del proxy per OTA update
-  "ota_proxy_user": "", // Username per autenticazione proxy
-  "ota_proxy_password": "", // Password per autenticazione proxy
+  "ota_proxy_user": "Sicheo-digil-gateway_acf23911-a7ae-4d11-a34d-296137d69c45@c62e55b3-65b7-4ea3-8b2f-ce5b3086b426", // Username per autenticazione proxy
+  "ota_proxy_password": "pLMoKN2024$$", // Password per autenticazione proxy
   "ota_tenant": "DEFAULT", // Tenant per OTA update
-  "ota_controller_id": "agent-1121621-PROTO-0006", // Controller ID per OTA update
+  "ota_controller_id": "acf23911-a7ae-4d11-a34d-296137d69c45", // Controller ID per OTA update
   "ota_poll_interval": 60 // Intervallo di polling per OTA update in secondi
 };
 
